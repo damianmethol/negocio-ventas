c0cc2f89261513fcad062d21e777ccbe06929694
Damian Methol <damianmethol@gmail.com>
2022-01-24 17:40:22 -0300

    Agregado LinksMB

---
+++ NegocioClient/src/main/java/ar/com/eduit/curso/java/web/managed/bean/LinksMB.java
@@ -0,0 +1,40 @@
+package ar.com.eduit.curso.java.web.managed.bean;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+
+@Named
+@SessionScoped
+public class LinksMB implements Serializable {
+    private String urlServer = "http://localhost:8080/NegocioClient/faces";
+    private String index = urlServer + "/index.xhtml";
+    private String clientes = urlServer + "/ClientesPrimefaces.xhtml";
+    private String articulos = urlServer + "/ArticulosPrimefaces.xhtml";
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(String clientes) {
+        this.clientes = clientes;
+    }
+
+    public String getArticulos() {
+        return articulos;
+    }
+
+    public void setArticulos(String articulos) {
+        this.articulos = articulos;
+    }
+    
+}
